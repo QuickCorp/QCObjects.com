b4a5dbc82226aa8295aa3dcac88ae53e5e77ff4d
Quickcorp <info@quickcorp.cl>
2020-03-26 23:30:21 -0300

    cached=true in markdown components

--- js/packages/org.quickcorp.custom.components.js
+++ js/packages/org.quickcorp.custom.components.js
@@ -44,16 +44,20 @@ Package('org.quickcorp.custom.components',[
     }
   }),
   Class('ReadmeMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
     name:"readmemarkdowncomponent",
     templateURI:"https://api.github.com/repos/QuickCorp/QCObjects/contents/README.md?ref=master"
   }),
   Class('AuthorMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
     templateURI:"https://api.github.com/repos/QuickCorp/QCObjects/contents/AUTHOR.md?ref=master"
   }),
   Class('ContributingMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
     templateURI:"https://api.github.com/repos/QuickCorp/QCObjects/contents/CONTRIBUTING.md?ref=master"
   }),
   Class('CoCMarkdownComponent',CustomMarkdownComponent,{
+    cached:true,
     templateURI:"https://api.github.com/repos/QuickCorp/QCObjects/contents/CODE_OF_CONDUCT.md?ref=master"
   })
 
